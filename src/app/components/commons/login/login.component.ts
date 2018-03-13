b5adf3000928332d00c8f0b2afbd175930c0edd1
刘强 <784248173@qq.com>
2018-03-13 18:48:56 +0800

    34343
    44444444444444444

--- src/app/components/commons/login/login.component.ts
+++ src/app/components/commons/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 import { Router } from '@angular/router';
 import {routerTransition} from '../../../router.animations';
+
+import  {User}  from '../../../models/systems/user/user.model';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +13,16 @@ import {routerTransition} from '../../../router.animations';
 })
 export class LoginComponent implements OnInit {
 
+  users:User[];
+  loginUser:User;
   constructor(public router: Router) {}
 
   ngOnInit() {
   }
 
-  onLoggedin() {
+  onLoggedin(f: NgForm) {
+    console.log(f)
+    console.log("=======================")
     localStorage.setItem('isLoggedin', 'true');
   }
 }
