16401dbfc94db927ac31ce831c99585dbe3e2a96
刘强 <784248173@qq.com>
2018-03-13 17:24:39 +0800

    0
    0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginCanActivateGuard } from './components/commons/guard/login-can-activate-guard';
+
+/**
+ *默认进入登录界面
+ * 点击登录，利用路由跳转到主界面
+  */
+
 const routes: Routes = [
   { path: '', loadChildren: './components/commons/login/login.module#LoginModule'}
-  //{ path: '', loadChildren: './components/commons/login/login.module#LoginModule', canActivate: [LoginCanActivateGuard] }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
