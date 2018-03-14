6f408c3ac79936b48274a95e963e16925bc44118
刘强 <784248173@qq.com>
2018-03-14 10:06:28 +0800

    双向绑定ok

---
+++ src/app/components/commons/login/login-user-model.ts
@@ -0,0 +1,13 @@
+//默认是public类型
+export class LoginUserModel {
+  constructor(
+    public name: string
+  ) {  }
+}
+// You can create a new hero like this:
+/*
+let myHero =  new LoginUserModel(42, 'liuqiang',
+  'Fetch any object at any distance');
+console.log('My hero is called ' + myHero.name);
+*/
+// "My hero is called SkyDog"
--- src/app/components/commons/login/login.component.ts
+++ src/app/components/commons/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+
 import { Router } from '@angular/router';
 import {routerTransition} from '../../../router.animations';
 
-import  {User}  from '../../../models/systems/user/user.model';
+import  {LoginUserModel}  from './login-user-model';
 
 @Component({
   selector: 'app-login',
@@ -13,16 +13,9 @@ import  {User}  from '../../../models/systems/user/user.model';
 })
 export class LoginComponent implements OnInit {
 
-  users:User[];
-  loginUser:User;
+  model = new LoginUserModel("asdfasfafaliuqiang");
   constructor(public router: Router) {}
 
   ngOnInit() {
-  }
-
-  onLoggedin(f: NgForm) {
-    console.log(f)
-    console.log("=======================")
-    localStorage.setItem('isLoggedin', 'true');
   }
 }
