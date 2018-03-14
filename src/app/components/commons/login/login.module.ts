2f1df5a07e859b452e4fc12fe3a3cfe38bcc980b
刘强 <784248173@qq.com>
2018-03-14 10:23:29 +0800

    双向绑定ok

--- src/app/components/commons/login/login.module.ts
+++ src/app/components/commons/login/login.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+//表单模块
 import { FormsModule }   from '@angular/forms';
+//路由模块
 import { LoginRoutingModule } from './login-routing.module';
-
+//登录组件
 import { LoginComponent } from './login.component';
 
 @NgModule({
   imports: [
-    CommonModule,
+    CommonModule,//NgIf、NgFor等指令
     LoginRoutingModule,
     FormsModule
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent]//模块内部Components/Directives/Pipes的列表，声明一下这个模块内部成员
 })
-export class LoginModule { }
+export class LoginModule { }//来控制将哪些内部成员暴露给外部使用
